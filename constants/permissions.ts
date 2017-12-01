092b8d04ce79bad18ee5e7bb26545910d525ab3e
Domenico Rizzo <dev@idsign.it>
2017-12-01 18:03:25 +0100

    Added constants

--- constants/permissions.ts
+++ constants/permissions.ts
@@ -1,7 +1,23 @@
 const STATE_ENABLED = 1;
 const STATE_DISABLED = 0;
 
+/**
+ * Canonical permissions
+ * */
+const READ = 'read';
+const CREATE = 'create';
+const UPDATE = 'update';
+const DELETE = 'delete';
+
 export {
     STATE_DISABLED,
-    STATE_ENABLED
+    STATE_ENABLED,
+
+    /**
+     * Canonical permissions
+     * */
+    READ,
+    CREATE,
+    UPDATE,
+    DELETE
 }
